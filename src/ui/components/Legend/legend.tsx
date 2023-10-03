37eb791f6179c17f72a6403b407611adf057ea76
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2023-10-03 09:48:42 +0100

    Update legend

--- src/ui/components/Legend/legend.tsx
+++ src/ui/components/Legend/legend.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, SimpleGrid} from "@chakra-ui/react";
+import { Text, Box, Center, SimpleGrid} from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
 import { StripToolConfig } from "../../../types";
@@ -6,20 +6,24 @@ import { StripToolConfig } from "../../../types";
 export const Legend = (): JSX.Element => {
     const legend: React.JSX.Element[] = [];
     const curves = useSelector((state: StripToolConfig) => state.curve);
-    console.log(curves);
-    curves.forEach((curve) => {
+    const colors = useSelector((state: StripToolConfig) => state.color.colors);
+    curves.forEach((curve, idx) => {
+      const color = `rgb(${colors[idx][0]},${colors[idx][1]},${colors[idx][2]})`;
         // For each one we create a legend item
         legend.push(
-            <Box>
-                <Center>
-                    testing text
+            <Box width={250}>
+                <Center  borderStyle={"solid"} borderTopWidth={2} borderBottomWidth={2} borderRightWidth={4} borderLeftWidth={4} borderColor={color}>
+                    {curve.name}
                 </Center>
-                {curve.max}
+                <Text align={"left"}> {`${curve.units}`} </Text>
+                <Text align={"left"}> {`${curve.scale ? "log10" : ""} (${curve.min}, ${curve.max}) VAL=${2}`}</Text>
+                <Text align={"left"}>{`${curve.comment}`}</Text>
+                
             </Box>
         )
     })
     return (
-        <SimpleGrid columns={1} spacing={2}>
+        <SimpleGrid float={"right"} columns={1} spacing={2}>
           {legend}
         </SimpleGrid>
     );
